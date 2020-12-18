e8c55771045e5185d6369bdfe75ee56373f28471
Wei Chen <weichen@apache.org>
2020-12-18 15:14:58 +0800

    code-init

---
+++ README.md
@@ -0,0 +1,48 @@
+# TrueML Report
+
+This is a testing playground for TrueML reporting generation process with Markdown Format.
+
+The goal of this test is to check what visualizations are available in the current markdown transformer.
+
+## List
+
+ - item 1
+ - item 2
+
+## Checkbox
+
+ - [ ] item 1
+ - [ ] item 2
+
+## Links
+
+![logo](https://avatars2.githubusercontent.com/u/74423449)
+
+[TrueML Link](https://github.com/trueml)
+
+## Table
+
+| Column 1 | Column 2 |
+| - | - |
+| A | B |
+
+## Font
+
+ - _Italic_
+ - **BOLD**
+ - ~~CROSS~~
+
+## Code
+
+`One Liner`
+
+``` js
+var foo = function (bar) {
+  return bar++;
+};
+console.log(foo(5));
+```
+
+## Html
+
+<div> TrueML Html </div>
---
+++ truemd.js
@@ -0,0 +1,27 @@
+var mdstr = "";
+var request = new XMLHttpRequest();
+request.open('GET', '/README.md', true);
+
+request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+        console.log("Markdown Loaded");
+        mdstr = this.response;
+    }
+};
+request.send();
+
+function gen() {
+    document.getElementById("cdiv").innerHTML = window.markdownit().render(mdstr);
+}
+function pri() {
+    let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+    mywindow.document.write('<html><head><title>TrueML Report</title>');
+    mywindow.document.write('</head><body >');
+    mywindow.document.write(document.getElementById("cdiv").innerHTML);
+    mywindow.document.write('</body></html>');
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+    mywindow.print();
+
+    return true;
+}
